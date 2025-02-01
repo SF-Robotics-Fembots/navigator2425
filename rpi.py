d0816ddee401941159782cadcab33badb5d98988
cynthia-k2 <0907cincinnati@gmail.com>
2025-02-01 15:08:48 -0800

    setblocking

--- rpi.py
+++ rpi.py
@@ -5,6 +5,7 @@ host_ip = '10.0.0.8'
 port = 8080
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.setblocking(False)
 client_socket.connect((host_ip, port))
 print("1")
 #s.sendall(b"hello, world")
@@ -36,7 +37,7 @@ while True:
     while True:
         datain = client_socket.recv(1024)
         print("DI", datain)
-        if not datain: break
+        if len(datain) < 5: break
         data = datain[-44:]
     
     #if not data: break
