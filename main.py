37b867af186ed99ee7a0fd114b44c7335f6debe3
cynthia-k2 <0907cincinnati@gmail.com>
2025-02-15 10:55:39 -0800

    trying again

--- main.py
+++ main.py
@@ -81,15 +81,15 @@ while running:
             added_values = int(axis_0_rotation + axis_2_rotation)
             print("x and r:", added_values)
 
-            # if (added_values > 100):
-            #     power_limit = added_values/(added_values/100)
-            #     print(power_limit)
-            # elif (added_values < 100):
-            #     pass
-            # elif (added_values == 100):
-            #     pass
-            # else:
-            #     print("something went wrong")
+            if (added_values > 100):
+                power_limit = added_values/(added_values/100)
+                print(power_limit)
+            elif (added_values < 100):
+                pass
+            elif (added_values == 100):
+                pass
+            else:
+                print("something went wrong")
 
             axis_0_pwm_value = joystick_to_pwm(axis_0)
             axis_1_pwm_value = joystick_to_pwm(axis_1)
