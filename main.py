69a074f992354e3a39c8581bc7ba62f356566492
cynthia-k2 <0907cincinnati@gmail.com>
2025-02-22 14:18:47 -0800

    changing from pwm_values being sent to thruster_values being sent to client side (going to have to reconfigure some code on bottomside)

--- main.py
+++ main.py
@@ -107,10 +107,10 @@ while running:
 
             thruster_pwm_values = [joystick_to_pwms(percentage) for percentage in final_percentage]
 
-            axis_x_pwm_value = joystick_to_pwm(axis_x)
-            axis_y_pwm_value = joystick_to_pwm(axis_y)
-            axis_r_pwm_value = joystick_to_pwm(axis_r)
-            axis_z_pwm_value = joystick_to_pwm(axis_z)
+            # axis_x_pwm_value = joystick_to_pwm(axis_x)
+            # axis_y_pwm_value = joystick_to_pwm(axis_y)
+            # axis_r_pwm_value = joystick_to_pwm(axis_r)
+            # axis_z_pwm_value = joystick_to_pwm(axis_z)
 
             # print(f"Raw Values: Axis 0: {axis_x}, Axis 1: {axis_y}, Axis 2:{axis_r}, Axis 3: {axis_z}")
             # print(f"Thruster %:", thruster_percent_ideal)
@@ -119,14 +119,22 @@ while running:
             print(f"PWM Thruster Values: ", thruster_pwm_values)
             # print(f"PWM Values: Axis 0: {axis_x_pwm_value}, Axis 1: {axis_r_pwm_value}, Axis 2: {axis_r_pwm_value}, Axis 3: {axis_z_pwm_value}")
 
-            pwm_values = {
-                'x': axis_x_pwm_value,
-                'y': axis_y_pwm_value,
-                'r': axis_r_pwm_value,
-                'v': axis_z_pwm_value
-            }
+            # pwm_values = {
+            #     'x': axis_x_pwm_value,
+            #     'y': axis_y_pwm_value,
+            #     'r': axis_r_pwm_value,
+            #     'v': axis_z_pwm_value
+            # }
 
-            json_data = json.dumps(pwm_values)
+            thruster_values = {
+                    'thruster_1': thruster_1_b,
+                    'thruster_2': thruster_2_b,
+                    'thruster_3': thruster_3_b,
+                    'thruster_4': thruster_4_b,
+                    'thruster_5': thruster_5_b
+                }
+
+            json_data = json.dumps(thruster_values)
             client_socket.sendall(json_data.encode('utf-8'))
 
             time.sleep(0.005)
