ef84e1a851d63f93efeb317f81e2299522efc645
cynthia-k2 <0907cincinnati@gmail.com>
2025-04-21 16:22:35 -0700

    test 2

--- test.py
+++ test.py
@@ -25,13 +25,13 @@ shield = adafruit_pca9685.PCA9685(i2c)
 kit = ServoKit(channels=16)
 shield.frequency = 100 #was 100
 
-thrusterChannel = []
+thrusterChannel = [shield.channels[14], shield.channels[1], shield.channels[8], shield.channels[15], shield.channels[2]]
 
-thrusterChannel[0] = shield.channels[14]
-thrusterChannel[1] = shield.channels[1]
-thrusterChannel[2] = shield.channels[8]
-thrusterChannel[3] = shield.channels[15]
-thrusterChannel[4] = shield.channels[2]
+# thrusterChannel[0] = shield.channels[14]
+# thrusterChannel[1] = shield.channels[1]
+# thrusterChannel[2] = shield.channels[8]
+# thrusterChannel[3] = shield.channels[15]
+# thrusterChannel[4] = shield.channels[2]
 
 pwm_values = [1000, 1500, 2000]
 
